41cf21009ca0e6e12a0a9babb68490ec3c0a81ee
Angelo Bruch <angelo.bruch@live.com>
2023-05-03 20:53:25 -0300

    Criação de rotas seguras personalização da header de login

--- javascript_note/src/routes.js
+++ javascript_note/src/routes.js
@@ -8,14 +8,16 @@ import Login from './screens/auth/login'
 import Notes from './screens/notes/index'
 import UserEdit from './screens/users/edit'
 
+import PrivateRouter from './components/auth/private_router'
+
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
-            <Route exact path='/notes' component={Notes} />
-            <Route exact path='/users/edit' component={UserEdit} />
+            <PrivateRouter exact path='/notes' component={Notes} />
+            <PrivateRouter exact path='/users/edit' component={UserEdit} />
         </Switch>
     </BrowserRouter>
 )
